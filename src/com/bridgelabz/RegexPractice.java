3c5288cf3eed76d8571e65eac8596bac0bad6aa7
Nagraj94 <ngowda335@gmail.com>
2022-08-01 22:08:23 +0530

    Validating phone number which starts from + or 91

--- src/com/bridgelabz/RegexPractice.java
+++ src/com/bridgelabz/RegexPractice.java
@@ -9,6 +9,7 @@ public class RegexPractice{
     static Scanner sc = new Scanner(System.in);
     static Pattern name = Pattern.compile("^[A-Z]\\w{3,}$");
     static Pattern email = Pattern.compile("^[a-z+.]+@(.+)$");
+    static Pattern phNo = Pattern.compile("^(?:(?:\\+|0{0,2})91(\\s*[\\ -]\\s*)?|[0]?)?[789]\\d{9}|(\\d[ -]?){10}\\d$");
 
     public static void main(String[] args) {
         System.out.println("Enter first name: ");
@@ -17,6 +18,8 @@ public class RegexPractice{
         String lastName = sc.next();
         System.out.println("eg.abc.xyz@bl.co.in\nEnter email: ");
         String emailValid = sc.next();
+        System.out.println("Enter phone number");
+        String phNoValid = sc.next();
 
         Matcher matchFirstName = name.matcher(firstName);
         Matcher matchLastName = name.matcher(lastName);
@@ -33,6 +36,12 @@ public class RegexPractice{
         else {
             System.out.println("enter valid email eg.eg.abc.xyz@bl.co.in");
         }
-
+        Matcher matchNum = phNo.matcher(phNoValid);
+        if (matchNum.matches()){
+            System.out.println("phone number is valid");
+        }
+        else {
+            System.out.println("enter phone number valid number");
+        }
     }
 }
