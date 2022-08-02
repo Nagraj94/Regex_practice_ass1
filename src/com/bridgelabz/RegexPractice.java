e92cecbab9876fa8bf6d4453ecf19197a044dbf4
Nagraj94 <ngowda335@gmail.com>
2022-08-02 09:36:53 +0530

    validating password by atleast one uppercase, lowercase and numbric values

--- src/com/bridgelabz/RegexPractice.java
+++ src/com/bridgelabz/RegexPractice.java
@@ -11,50 +11,48 @@ public class RegexPractice{
     static Pattern email = Pattern.compile( "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
             + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$");
     static Pattern phNo = Pattern.compile("^(?:(?:\\+|0{0,2})91(\\s*[\\ -]\\s*)?|[0]?)?[789]\\d{9}|(\\d[ -]?){10}\\d$");
-    static Pattern passWord = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z]).{8,20}$");
+    static Pattern passWord = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])+(?=.*[0-9]).{8,20}$");
 
     public static void main(String[] args) {
-//        System.out.println("Enter first name: ");
-//        String firstName = sc.next();
-//        System.out.println("Enter last name: ");
-//        String lastName = sc.next();
-//        System.out.println("eg.abc.xyz@bl.co.in\nEnter email: ");
-//        String emailValid = sc.next();
-//        System.out.println("Enter phone number");
-//        String phNoValid = sc.next();
-//
-//        Matcher matchFirstName = name.matcher(firstName);
-//        Matcher matchLastName = name.matcher(lastName);
-//        if (matchFirstName.matches() && matchLastName.matches()){
-//            System.out.println("Valid");
-//        }
-//        else {
-//            System.out.println("first letter should start with capital");
-//        }
-//        Matcher matchEmail = email.matcher(emailValid);
-//        if (matchEmail.matches()) {
-//            System.out.println("email is valid");
-//        } else {
-//            System.out.println("enter valid email eg.eg.abc.xyz@bl.co.in");
-//        }
-//        Matcher matchNum = phNo.matcher(phNoValid);
-//        if (matchNum.matches()){
-//            System.out.println("phone number is valid");
-//        }
-//        else {
-//            System.out.println("enter valid phone number");
-//        }
-        while (true){
-            System.out.println("Enter password: ");
-            String passValid = sc.next();
-            Matcher matchPassword = passWord.matcher(passValid);
-            if (matchPassword.matches()){
-                System.out.println("password is valid ");
-            }
-            else {
-                System.out.println("password is invalid");
-            }
+        System.out.println("Enter first name: ");
+        String firstName = sc.next();
+        System.out.println("Enter last name: ");
+        String lastName = sc.next();
+        System.out.println("eg.abc.xyz@bl.co.in\nEnter email: ");
+        String emailValid = sc.next();
+        System.out.println("Enter phone number");
+        String phNoValid = sc.next();
+        System.out.println("Enter password: ");
+        String passValid = sc.next();
+
+        Matcher matchFirstName = name.matcher(firstName);
+        Matcher matchLastName = name.matcher(lastName);
+        if (matchFirstName.matches() && matchLastName.matches()){
+            System.out.println("Valid");
+        }
+        else {
+            System.out.println("first letter should start with capital");
+        }
+        Matcher matchEmail = email.matcher(emailValid);
+        if (matchEmail.matches()) {
+            System.out.println("email is valid");
+        } else {
+            System.out.println("enter valid email eg.eg.abc.xyz@bl.co.in");
+        }
+        Matcher matchNum = phNo.matcher(phNoValid);
+        if (matchNum.matches()){
+            System.out.println("phone number is valid");
+        }
+        else {
+            System.out.println("enter valid phone number");
         }
 
+        Matcher matchPassword = passWord.matcher(passValid);
+        if (matchPassword.matches()){
+            System.out.println("password is valid ");
+        }
+        else {
+            System.out.println("password is invalid");
+        }
     }
 }
